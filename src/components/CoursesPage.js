6297353f116c036bd80e479db4e49005755633bf
pavlosmcg <pavlosmcg@users.noreply.github.com>
2020-11-06 19:10:36 +0000

    Compose course list of smaller, smart & dumb components

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
@@ -12,26 +13,7 @@ function CoursesPage() {
   return (
     <>
       <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author ID</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map((course) => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <CourseList courses={courses} />
     </>
   );
 }
