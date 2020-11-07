7e31d0ba8efae722a5026ddb74d500f265ff74e6
pavlosmcg <pavlosmcg@users.noreply.github.com>
2020-11-07 15:02:58 +0000

    Example of a redirect for old routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="/about" />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
