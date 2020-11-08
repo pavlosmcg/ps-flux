91809df83b62852c66c4c3b311ad3f888bbe34dc
pavlosmcg <pavlosmcg@users.noreply.github.com>
2020-11-08 19:40:45 +0000

    And fix the new courses having empty author names too!

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -6,34 +6,39 @@ import * as authorApi from "../api/authorApi";
 // action creator
 export function saveCourse(course) {
   return courseApi.saveCourse(course).then((savedCourse) => {
-    dispatcher.dispatch(
-      // action itself
-      {
-        actionType: course.id
-          ? actionTypes.UPDATE_COURSE
-          : actionTypes.CREATE_COURSE,
-        course: savedCourse,
-      }
-    );
+    authorApi.getAuthors().then((authors) => {
+      dispatcher.dispatch(
+        // action itself
+        {
+          actionType: course.id
+            ? actionTypes.UPDATE_COURSE
+            : actionTypes.CREATE_COURSE,
+          course: updateCourseWithAuthorName(savedCourse, authors),
+        }
+      );
+    });
   });
 }
 
 export function loadCourses() {
   return courseApi.getCourses().then((courses) => {
     authorApi.getAuthors().then((authors) => {
-      const coursesWithAuthorNames = courses.map((course) => {
-        // find the author from the authors api response:
-        const author = authors.find((author) => author.id === course.authorId);
-        // attach the author name to the course object:
-        return {
-          ...course,
-          authorName: author.name,
-        };
-      });
       dispatcher.dispatch({
         actionType: actionTypes.LOAD_COURSES,
-        courses: coursesWithAuthorNames,
+        courses: courses.map((course) =>
+          updateCourseWithAuthorName(course, authors)
+        ),
       });
     });
   });
+}
+
+function updateCourseWithAuthorName(course, authors) {
+  // find the author from the authors api response:
+  const author = authors.find((author) => author.id === course.authorId);
+  // attach the author name to the course object:
+  return {
+    ...course,
+    authorName: author.name,
+  };
 }
